8d160aeab452c0d63744294e3f286592a6f1c1d4
Nikolay <halemhood@icloud.com>
2022-02-16 14:16:46 +0300

    final editing

--- NavigationApp/ThirdViewController.swift
+++ NavigationApp/ThirdViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    
+    var text: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        textField.text = text
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeVC() {
+        text = textField.text
     }
-    */
-
+    /*
+     если не нужно передавать данные исп просто дисмисс
+     провести астион от кнопки в его класс
+     если нужно передатьб удаляем дисмисс
+     в предидущем окне получателе данных
+     пишем  @IBAction func unwindSegue(segue: UIStoryboardSegue) {}
+     в сториборд протягиваем от кнопки в экзит на этом окне
+     */
+    
+    
+    deinit {
+        print("Del third")
+    }
 }
